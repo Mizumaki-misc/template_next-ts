c0caf63ae2425da6b99ce482cc17987720631594
Ryota Mizumaki <elon.statham@gmail.com>
2022-05-21 14:56:49 +0900

    add postcss plugins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hello } from "~/components/Hello";
+import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className={styles.title}>Home</h1>
+      <p>Statham</p>
       <Hello />
     </div>
   );
