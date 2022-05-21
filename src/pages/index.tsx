edb8516ab138048b2e67d9d6dde558475a7268f0
Ryota Mizumaki <elon.statham@gmail.com>
2022-05-21 15:56:09 +0900

    add tailwind CSS and headless UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Hello } from '~/components/Hello';
 import styles from './Home.module.css';
+import { MyDisclosure } from '~/components/MyDisclosure';
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1 className={styles.title}>Home</h1>
-      <p>Statham</p>
+      <p className='text-3xl font-bold'>Statham</p>
       <Hello />
+      <MyDisclosure />
     </div>
   );
 };
